4eaf257e083172a87f70d98b340b363f152b831f
Justin Huang <jstn@cs.washington.edu>
2017-04-09 20:26:12 -0700

    Initial Torso class and demo code.

---
+++ fetch_api/src/fetch_api/torso.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import rospy
+
+TIME_FROM_START = 5  # How many seconds it should take to set the torso height.
+
+
+class Torso(object):
+    """Torso controls the robot's torso height.
+    """
+    MIN_HEIGHT = 0.0
+    MAX_HEIGHT = 0.4
+
+    def __init__(self):
+        pass
+
+    def set_height(self, height):
+        """Sets the torso height.
+
+        This will always take ~5 seconds to execute.
+
+        Args:
+            height: The height, in meters, to set the torso to. Values range
+                from Torso.MIN_HEIGHT (0.0) to Torso.MAX_HEIGHT(0.4).
+        """
+        rospy.logerr('Not implemented.')
