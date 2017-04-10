a800e08df486f9ae0b85bd5ed02453787afdaf76
Justin Huang <jstn@cs.washington.edu>
2017-04-09 19:06:12 -0700

    Updated minimum grasp force.

--- fetch_api/src/fetch_api/gripper.py
+++ fetch_api/src/fetch_api/gripper.py
@@ -9,7 +9,7 @@ OPENED_POS = 0.10  # The position for a fully-open gripper (meters).
 class Gripper(object):
     """Gripper controls the robot's gripper.
     """
-    MIN_EFFORT = 45  # Min grasp force, in Newtons
+    MIN_EFFORT = 35  # Min grasp force, in Newtons
     MAX_EFFORT = 100  # Max grasp force, in Newtons
 
     def __init__(self):
@@ -25,6 +25,6 @@ class Gripper(object):
 
         Args:
             max_effort: The maximum effort, in Newtons, to use. Note that this
-                should not be less than 45N, or else the gripper may not close.
+                should not be less than 35N, or else the gripper may not close.
         """
         rospy.logerr('Not implemented.')
