5c8e389d31aa5f6a85631d2d35dd8f41ce7995f8
Team 4 <michael.shintaku1@gmail.com>
2017-05-29 12:21:58 -0700

    Commandline robot stuff skeleton

---
+++ library_bot/nodes/robot_cmdline_runner.py
@@ -0,0 +1,22 @@
+from main_runner import BookServer
+
+def main():
+    inpt = None
+    while True:
+        print "Type: (home) for the home positions, x: [1, 4]."
+        print "Type: (bookShelf) for the bookshelf positions, y: [1, 4]"
+        inpt = raw_input()
+        bookServ = BookServer()
+        if inpt == "home":
+            print "type number 1-4"
+            inpt = int(raw_input)
+            book_id = -inpt
+            
+
+        else:
+            print "type number 1-4"
+            inpt = int(raw_input)
+            book_id = inpt
+
+if __name__ == '__main__':
+    main()
